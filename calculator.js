5a53ef298bf49ee58761378941ad255c40afd797
T-Lime <tilimedn@gmail.com>
2026-07-13 15:32:06 +0900

    refactor : calculator.js

--- calculator.js
+++ calculator.js
@@ -1,7 +1,13 @@
 //더하기
-const add = (a, b) => a + b;
+const add = (a, b) => {
+  return a + b;
+};
 
 // 뺄셈
-const subtract = (a, b) => a - b;
+const subtract = (a, b) => {
+  return a - b;
+};
 
-const multiply = (a, b) => a * b;
+const multiply = (a, b) => {
+  return a * b;
+};
