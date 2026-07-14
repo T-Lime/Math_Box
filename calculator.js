97548c1bb1ec631bf85f9cedf00b44e78be23ee7
T-Lime <tilimedn@gmail.com>
2026-07-14 11:23:33 +0900

    add comments

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-//더하기
+// 덧셈
 const add = (a, b) => {
   return a + b;
 };
@@ -8,10 +8,12 @@ const subtract = (a, b) => {
   return a - b;
 };
 
+// 곱셈
 const multiply = (a, b) => {
   return a * b;
 };
 
+// 나눗셈
 const divide = (a, b) => {
   return a / b;
 };
